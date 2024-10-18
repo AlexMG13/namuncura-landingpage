f216d59d74a6b0e0c751f9e37bc844966506eb28
Alex <mendozaalex24@gmail.com>
2024-10-18 08:44:43 -0300

    change images and class

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			primary: '#128a64',
+			secondary: '#f2e79d',
+		  }
 	},
 	plugins: [],
 }
